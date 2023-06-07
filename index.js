e66544210631bb4184e1028801ece88bebeecaec
Nguyễn Quang Dũng - Intern <dunghlhh@gmail.com>
2023-06-07 11:42:28 +0700

    update

--- index.js
+++ index.js
@@ -37,13 +37,6 @@ app.get("/", (req, res) => {
 });
 
 // routing
-app.use(function (req, res, next) {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    next();
-});
 app.use(PromptRouter, function (req, res, next) {
     next();
 });
